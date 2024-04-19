ae94d4413fed104cac528dc10842d48a65ad5a5a
melinahernandeze <147454229+melinahernandeze@users.noreply.github.com>
2024-04-18 20:12:15 -0400

    Add files via upload

---
+++ test.py
@@ -0,0 +1,41 @@
+renter or owner,household income,age,total
+owner,Less than $10,000,Under 25 years old,42
+owner,Less than $10,000,25 to 29 years old,50
+owner,Less than $10,000,30 to 34 years old,109
+owner,Less than $10,000,35 to 44 years old,353
+owner,Less than $10,000,45 to 54 years old,527
+owner,Less than $10,000,55 to 64 years old,1091
+owner,Less than $10,000,65 to 74 years old,1155
+owner,Less than $10,000,75 years old and over,1162
+owner,$10,000 to $19,999,Under 25 years old,S
+owner,$10,000 to $19,999,25 to 29 years old,31
+owner,$10,000 to $19,999,30 to 34 years old,61
+owner,$10,000 to $19,999,35 to 44 years old,274
+owner,$10,000 to $19,999,45 to 54 years old,369
+owner,$10,000 to $19,999,55 to 64 years old,883
+owner,$10,000 to $19,999,65 to 74 years old,1361
+owner,$10,000 to $19,999,75 years old and over,1637
+owner,$20,000 to $29,999,Under 25 years old,59
+owner,$20,000 to $29,999,25 to 29 years old,107
+owner,$20,000 to $29,999,30 to 34 years old,156
+owner,$20,000 to $29,999,35 to 44 years old,369
+owner,$20,000 to $29,999,45 to 54 years old,489
+owner,$20,000 to $29,999,55 to 64 years old,1056
+owner,$20,000 to $29,999,65 to 74 years old,1666
+owner,$20,000 to $29,999,75 years old and over,1737
+owner,$30,000 to $39,999,Under 25 years old,68
+owner,$30,000 to $39,999,25 to 29 years old,184
+owner,$30,000 to $39,999,30 to 34 years old,120
+owner,$30,000 to $39,999,35 to 44 years old,502
+owner,$30,000 to $39,999,45 to 54 years old,654
+owner,$30,000 to $39,999,55 to 64 years old,1250
+owner,$30,000 to $39,999,65 to 74 years old,1413
+owner,$30,000 to $39,999,75 years old and over,1342
+towner,$40,000 to $49,999,Under 25 years old,79
+owner,$40,000 to $49,999,25 to 29 years old,220
+owner,$40,000 to $49,999,30 to 34 years old,306
+owner,$40,000 to $49,999,35 to 44 years old,601
+owner,$40,000 to $49,999,45 to 54 years old,872
+owner,$40,000 to $49,999,55 to 64 years old,1093
+owner,$40,000 to $49,999,65 to 74 years old,1424
+owner,$40,000 to $49,999,75 years old and over,1180
